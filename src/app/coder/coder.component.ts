3b7bf173754df64c2189fc43ad0ae559e280e1b3
michal.dydo <michal.dydo@rnt.com.pl>
2017-04-05 13:55:41 +0200

    working on TS file

--- src/app/coder/coder.component.ts
+++ src/app/coder/coder.component.ts
@@ -11,27 +11,45 @@ export class CoderComponent implements OnInit {
   private collectionHx: any = [];
   private collectionGx: any;
 
+  private binaryScope: string[] = ['0', '1'];
+  private binaryRange= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  private selectedBinaryScope0 = '0';
+  private selectedBinaryScope1 = 0;
+  private selectedBinaryScope2 = 0;
+  private selectedBinaryScope3 = 0;
+  private selectedBinaryScope4 = 0;
+  private selectedBinaryScope5 = 0;
+  private selectedBinaryScope6 = 0;
+  private selectedBinaryScope7 = 0;
+  private selectedBinaryScope8 = 0;
+  private selectedBinaryScope9 = 0;
+
+
   constructor() { }
 
-  setInputsHx() {
-    for (let i = 0; i < this.hx; i++) {
-      this.collectionHx.push(i);
-    }
-  }
+  // setInputsHx() {
+  //   for (let i = 0; i < this.hx; i++) {
+  //     this.collectionHx.push(i);
+  //   }
+  // }
 
-  setInputsGx() {
-    this.collectionGx = [];
-    for (let i = 0; i < this.gx - 1; i++) {
-      this.collectionGx.push(i);
-    }
+  // setInputsGx() {
+  //   this.collectionGx = [];
+  //   for (let i = 0; i < this.gx - 1; i++) {
+  //     this.collectionGx.push(i);
+  //   }
+  // }
+
+  changeState() {
+    console.log('this.selectedBinaryScope0', this.selectedBinaryScope0);
   }
 
   ngOnInit() {
-    this.setInputsHx();
-    this.setInputsGx();
+    // this.setInputsHx();
+    // this.setInputsGx();
 
-    let content: any = ( < HTMLInputElement > document.getElementById('lastgxword'));
-    console.log(content.value);
+    // let content: any = ( < HTMLInputElement > document.getElementById('lastgxword'));
+    // console.log(content.value);
   }
 
 }
