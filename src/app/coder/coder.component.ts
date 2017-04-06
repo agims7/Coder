a3301df26f5dbc709af812631cda020e9c8cfe14
michal.dydo <michal.dydo@rnt.com.pl>
2017-04-06 14:58:00 +0200

    dalsze prace nad inputami

--- src/app/coder/coder.component.ts
+++ src/app/coder/coder.component.ts
@@ -6,38 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coder.component.less']
 })
 export class CoderComponent implements OnInit {
-  private hx: number = 5;
-  private gx: number = 5;
-  private collectionHx: any = [];
-  private collectionGx: any;
-
+  private visibility = false;
   private binaryScope: string[] = ['0', '1'];
   private binaryRange= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   private selectedBinaryScope = ["0", "0", "0", "0", "0","0", "0", "0", "0", "0"];
 
-
   constructor() { }
 
-  // setInputsHx() {
-  //   for (let i = 0; i < this.hx; i++) {
-  //     this.collectionHx.push(i);
-  //   }
-  // }
-
-  // setInputsGx() {
-  //   this.collectionGx = [];
-  //   for (let i = 0; i < this.gx - 1; i++) {
-  //     this.collectionGx.push(i);
-  //   }
-  // }
-
+  calculate() {
+    this.selectedBinaryScope.reverse();
+    this.visibility = true;
+  }
 
   ngOnInit() {
-    // this.setInputsHx();
-    // this.setInputsGx();
 
-    // let content: any = ( < HTMLInputElement > document.getElementById('lastgxword'));
-    // console.log(content.value);
   }
 
 }
